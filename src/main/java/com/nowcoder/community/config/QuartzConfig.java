b3f644fc6dd9dd1510a712fd34782dcf1c850add
Mrkegang <Mrkegang@outlook.com>
2022-06-27 08:01:03 +0800

    完成热帖排行功能的代码编写和功能测试

---
+++ src/main/java/com/nowcoder/community/config/QuartzConfig.java
@@ -0,0 +1,49 @@
+package com.nowcoder.community.config;
+
+import com.nowcoder.community.quartz.PostScoreRefreshJob;
+import org.quartz.JobDataMap;
+import org.quartz.JobDetail;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.quartz.JobDetailFactoryBean;
+import org.springframework.scheduling.quartz.SimpleTriggerFactoryBean;
+
+/**
+ * @author kg
+ * @version 1.0
+ * @date 2022/6/8 21:57
+ */
+// 配置 -> 第一次读取，初始化到数据库 -> 以后，访问数据库调用任务，而不访问配置文件
+@Configuration
+public class QuartzConfig {
+
+    // FactoryBean可简化Bean的实例化过程：
+    // 1.通过FactoryBean封装Bean的实例化过程.
+    // 2.将FactoryBean装配到Spring容器里.
+    // 3.将FactoryBean注入给其他的bean.
+    // 4.该Bean得到的是FactoryBean所管理的对象实例.
+
+    // 刷新帖子分数
+    @Bean
+    public JobDetailFactoryBean postScoreRefreshJobDetail() {
+        JobDetailFactoryBean factoryBean = new JobDetailFactoryBean();
+        factoryBean.setJobClass(PostScoreRefreshJob.class);
+        factoryBean.setName("postScoreRefreshJob");
+        factoryBean.setGroup("communityJobGroup");
+        factoryBean.setDurability(true);
+        factoryBean.setRequestsRecovery(true);
+        return factoryBean;
+    }
+
+    @Bean
+    public SimpleTriggerFactoryBean postScoreRefreshTrigger(JobDetail postScoreRefreshJobDetail) {
+        SimpleTriggerFactoryBean factoryBean = new SimpleTriggerFactoryBean();
+        factoryBean.setJobDetail(postScoreRefreshJobDetail);
+        factoryBean.setName("postScoreRefreshTrigger");
+        factoryBean.setGroup("communityTriggerGroup");
+        factoryBean.setRepeatInterval(1000 * 60 * 5);
+        factoryBean.setJobDataMap(new JobDataMap());
+        return factoryBean;
+    }
+
+}
